fe7002a76fa1887f39ae8e9cd0390222f93ba89e
Kieron Messer <kieron.messer@digitalbarriers.com>
2016-07-22 14:35:48 +0100

    file to make a simple package

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-import os
+from setuptools import setup
+
+setup(
+    name = 'videoai',
+    version='4.0',
+    description='An SDK for accessing VideoAI',
+    keywords='video processing, machine learning, security, analytics, face-recognition',
+    license='Commercial',
+    author='Kieron Messer',
+    author_email='kieron.messer@digitalbarriers.com',
+    packages =['videoai',
+              'videoai.recognition',
+              ],
+    platforms='any',
+    install_requires=[
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Video Processing :: Analytics',
+        ],
+)
